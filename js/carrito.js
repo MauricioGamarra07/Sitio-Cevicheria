dc147c3058a26d3af59e6750bc006a625995e51c
MauricioGamarra07 <mauriciogamarra01@gmail.com>
2022-10-02 20:57:14 -0500

    calculando precio Total

--- js/carrito.js
+++ js/carrito.js
@@ -1,22 +1,7 @@
 class Carrito {
     //muestra producto seleccionado en carrito
-    insertarCarrito(articulo, cant) {
-        const row = document.createElement('tr');
-        /* row.classList.add('articulo'); */
-        row.innerHTML = `
-            <td data-label="Imagen"><img src="${articulo.img}" style="width: 150px; height:80px"></td>
-            <td data-label="Plato"><h4>${articulo.nombre}</h4></td>
-            <td data-label="Precio"><h4>${articulo.precio}</h4></td>
-            <td data-label="Cantidad"><h4>${cant}</h4></td>
-            <td data-label="Total"><h4>${articulo.precio * cant}</h4></td>
-            <td data-label="Eliminar"><span class="fa fa-trash eliminar" id="eliminar${articulo.id}"></span></td>
-        `;
 
-        listaProductos.appendChild(row);
-
-    }
-
-    actualizarCarrito(arreglo) {
+    insertarCarrito(arreglo) {
         listaProductos.innerHTML = ""; //Cada vez que yo llame a actualizarCarrito, lo primero q hago
         //es borrar el nodo. Y despues recorro el array lo actualizo de nuevo y lo rellena con la info
         //actualizado
@@ -24,13 +9,14 @@ class Carrito {
             const row = document.createElement('tr');
             /* row.classList.add('articulo'); */
             row.innerHTML = `
-            <td data-label="Imagen"><img src="${prod[1]}" style="width: 150px; height:80px"></td>
-            <td data-label="Plato"><h4>${prod[2]}</h4></td>
-            <td data-label="Precio"><h4>${prod[3]}</h4></td>
-            <td data-label="Cantidad"><h4>${prod[4]}</h4></td>
-            <td data-label="Total"><h4>${prod[3] * prod[4]}</h4></td>
-            <td data-label="Eliminar"><span class="fa fa-trash eliminar" id="eliminar${prod[0]}"></span></td>
-        `;
+                <td data-label="Imagen"><img src="${prod[1]}" style="width: 150px; height:80px"></td>
+                <td data-label="Plato"><h4>${prod[2]}</h4></td>
+                <td data-label="Precio"><h4>${prod[3]}</h4></td>
+                <td data-label="Cantidad"><h4>${prod[4]}</h4></td>
+                <td data-label="Total"><h4>${prod[3] * prod[4]}</h4></td>
+                <td data-label="Eliminar"><span class="fa fa-trash eliminar" id="eliminar${prod[0]}"></span></td>
+            `;
+
             listaProductos.appendChild(row);
 
             /* localStorage.setItem('carrito', JSON.stringify(arregloCarrito)); */
--- js/pedido.js
+++ js/pedido.js
@@ -16,8 +16,11 @@ const carro = new Carrito();
 const agregar = document.querySelectorAll(".agregar");
 const listaProductos = document.querySelector(".info-compra");
 const vaciarCarrito = document.getElementById("limpiar");
+const eliminarArticulo = document.querySelectorAll(".eliminar");
 
 const contadorCarrito = document.getElementById("contador");
+
+const precioTotal = document.querySelector(".precioTotal");
 
 let arregloCarrito = [];
 
@@ -37,14 +40,14 @@ function cargarEventos() {
     let newValue = 1;
     for (let i = 0; i < aumentar.length; i++) {
         /* alert("Hizo click en aumentar"); */
-        aumentar[i].addEventListener('click',function(event){
-                var buttonClicked = event.target;
-                /* console.log(buttonClicked); */
-                var input = buttonClicked.parentElement.children[1];
-                /* console.log(input); */
-                var inputValue = input.value;
-                newValue = parseInt(inputValue) + 1;
-                input.value = newValue;
+        aumentar[i].addEventListener('click', function (event) {
+            var buttonClicked = event.target;
+            /* console.log(buttonClicked); */
+            var input = buttonClicked.parentElement.children[1];
+            /* console.log(input); */
+            var inputValue = input.value;
+            newValue = parseInt(inputValue) + 1;
+            input.value = newValue;
 
             /* c++;
             c = (c < 10) ? "0" + c : c;
@@ -54,16 +57,16 @@ function cargarEventos() {
     }
     for (let i = 0; i < disminuir.length; i++) {
         /* alert("Hizo click en disminuir"); */
-        disminuir[i].addEventListener('click',function(event){
-                var buttonClicked = event.target;
-                var input = buttonClicked.parentElement.children[1];
-                var inputValue = input.value;
-                newValue = parseInt(inputValue) - 1;
-                if (newValue >= 0){
-                    input.value = newValue;
-                }else{
-                    alert('La cantidad debe ser mayor a 0');
-                }
+        disminuir[i].addEventListener('click', function (event) {
+            var buttonClicked = event.target;
+            var input = buttonClicked.parentElement.children[1];
+            var inputValue = input.value;
+            newValue = parseInt(inputValue) - 1;
+            if (newValue >= 0) {
+                input.value = newValue;
+            } else {
+                alert('La cantidad debe ser mayor a 0');
+            }
             /* if (c > 1){
                 c--;
                 c = (c < 10) ? "0" + c : c;
@@ -73,35 +76,49 @@ function cargarEventos() {
         });
     }
 
-
+    let total = 0;
+    let totalCompra = 0;
     let contador = 0;
-    let array = [0];
+    let array = [0]; //Array de los Id's
     for (let i = 0; i < agregar.length; i++) {
-        agregar[i].addEventListener('click', (e) => {
+        agregar[i].addEventListener('click', () => {
             let cantidad = parseInt(newValue);
             /* console.log(cantidad); */
 
+            //Capturamos el numero de id que está en su atributo id
             let id = parseInt(agregar[i].getAttribute('id'));
             console.log(id);
 
+            //Buscamos en el array un número igual que el id capturado
             let num = array.find(item => item == id);
             console.log(num);
 
-            let arregloProducto = [];
+            let arregloProducto = []; //Array donde agregaremos los productos 1 por 1
 
-            if (num == id){
+
+            /* Si el numero buscado en el array de Id's es igual al id
+            capturado significa que ya se agregó el producto, por ello
+            solo le sumamos la cantidad */
+            if (num == id) {
                 console.log("Ya se agregó");
-                for (let i = 0; i < arregloCarrito.length; i++){
-                    if (arregloCarrito[i][0] == id){
+                /* Recorremos el arreglo del Carrito y buscamos que id es igual al id 
+                capturado, una vez encontrado estaremos dentro del arreglo del Producto
+                y solo faltaría sumarle las cantidades */
+                for (let i = 0; i < arregloCarrito.length; i++) {
+                    if (arregloCarrito[i][0] == id) {
                         arregloCarrito[i][4] += cantidad;
                     }
                     console.log(arregloCarrito);
                 }
-                carro.actualizarCarrito(arregloCarrito);
-            }else{
+                carro.insertarCarrito(arregloCarrito);
+            }
+            /* Si el número de id no está en el array, le agregamos el
+            producto con sus datos en un array, y luego lo agregamos
+            en el array del carrito de compras */
+            else {
                 array.push(id);
                 array.sort();
-                contador ++;
+                contador++;
                 contadorCarrito.style.display = "block";
                 contadorCarrito.textContent = contador;
                 arregloProducto.push(infoProductos[i]["id"]);
@@ -109,12 +126,24 @@ function cargarEventos() {
                 arregloProducto.push(infoProductos[i]["nombre"]);
                 arregloProducto.push(infoProductos[i]["precio"]);
                 arregloProducto.push(cantidad);
+
                 arregloCarrito.push(arregloProducto);
+
                 /* console.log(arregloProducto); */
                 console.log(arregloCarrito);
-                carro.insertarCarrito(infoProductos[id - 1], cantidad);
+                carro.insertarCarrito(arregloCarrito);
             }
             console.log(array);
+
+            /* Usamos un for para Calcular el Precio Total, recorrienco todos los
+                arreglos de productos y multiplicando su precio y cantidad */
+            totalCompra = 0;
+            for (let i = 0; i < arregloCarrito.length; i++) {
+                totalCompra += arregloCarrito[i][3] * arregloCarrito[i][4];
+            }
+            precioTotal.textContent = `S/ ${totalCompra}`;
+            console.log(totalCompra);
+
             /* console.log(contador); */
             /* console.log(infoArticulos[i]); */
 
@@ -125,7 +154,16 @@ function cargarEventos() {
         });
     }
 
+    for (let i = 0; i < eliminarArticulo.length; i++) {
+        eliminarArticulo[i].addEventListener('click', () => {
+            console.log("Click en eliminar");
+        })
+    }
+
     vaciarCarrito.addEventListener("click", (e) => {
+        contador = 0;
+        /* console.log(contador); */
+        contadorCarrito.textContent = contador;
         carro.vaciarCarrito(e);
     });
 
